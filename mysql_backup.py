f09eabad1adf97453ff6b949991566e68fb4c49c
Kunal Bhandari <kunal_nitk@yahoo.com>
2020-11-01 16:26:30 -0600

    Added validation to check inputs

--- mysql_backup.py
+++ mysql_backup.py
@@ -2,8 +2,10 @@
 # coding: utf-8
 
 ###################### Inputs ############################
-DAYS_TO_KEEP = 1
-BUFFER_DAYS = 0.25
+FREQUENCY_DAYS = 1
+# To avoid consdering archives that took long time to backup
+# but are recent as old files
+BUFFER_DAYS = 1/24 
 
 COMPRESS = True
 BACKUP_PATH='/media/kunal/KBServerBkup/mysql'
@@ -14,7 +16,6 @@ time_format = "%Y%m%d%H%M%S"
 ###################### Libraries ############################
 import sys
 import os
-import time
 import re
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
 
@@ -24,20 +25,26 @@ from common_py.functions import *
 
 start_time = time.time()
 
-# Old backups to delete
-old_file_time = time.time() - DAYS_TO_KEEP * 24 * 60 * 60 - BUFFER_DAYS * 24 * 60 * 60
-old_file_time = time.strftime(time_format, time.localtime(old_file_time))
+if not os.path.exists(BACKUP_PATH):
+    raise ValueError(f"Backup path [{BACKUP_PATH}] doesn't exist... Exiting...")
 
+
+# Time to identify old backups
+recent_file_time = time.time() - FREQUENCY_DAYS * 24 * 60 * 60
+recent_file_time = time.strftime(time_format, time.localtime(recent_file_time))
+
+
+# Check if there is a backup more recent than above time
 current_files = os.listdir(BACKUP_PATH)
-old_files = [f for f in current_files
+
+new_files = [f for f in current_files
              if re.search(".*_(\\d+)\\..*", f)
-                 and re.search(".*_(\\d+)\\..*", f).group(1) < old_file_time]
-for f in old_files:
-    print(f"mysql_backup.py: Deleting old file [{f}]")
-    try:
-        os.system(f" rm '{os.path.join(BACKUP_PATH, f)}'")
-    except:
-        print(f"mysql_backup.py: Error deleting old file [{f}]")
+                 and re.search(".*_(\\d+)\\..*", f).group(1) >= recent_file_time]
+
+if len(new_files) > 0:
+    print(f"mysql_backup.py: Recent backups found [{new_files}]\nExiting...")
+    sys.exit(0)
+
 
 # Databases to backup
 databases = set(run_sql("SHOW DATABASES")["Database"]) - set(SYSTEM_DATABASES)
@@ -61,6 +68,23 @@ except:
 
 print("mysql_backup.py: All databases successfully backed up")
 
+
+# Delete old backups
+old_file_time = time.time() - FREQUENCY_DAYS * 24 * 60 * 60 - BUFFER_DAYS * 24 * 60 * 60
+old_file_time = time.strftime(time_format, time.localtime(old_file_time))
+
+old_files = [f for f in current_files
+             if re.search(".*_(\\d+)\\..*", f)
+                 and re.search(".*_(\\d+)\\..*", f).group(1) < old_file_time]
+for f in old_files:
+    print(f"mysql_backup.py: Deleting old file [{f}]")
+    try:
+        os.system(f" rm '{os.path.join(BACKUP_PATH, f)}'")
+    except:
+        print(f"mysql_backup.py: Error deleting old file [{f}]")
+
+
+# Final reports to email
 finish_time = time.time()
 print(f"mysql_backup.py: Time to back up [{(finish_time - start_time) / 3600} hours]")
 
