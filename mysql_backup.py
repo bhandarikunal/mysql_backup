bf7b7eae61a99645ee64e28d76744463567741cc
Kunal Bhandari <kunal_nitk@yahoo.com>
2020-11-27 16:47:56 -0600

    Added flexibility to maintain multiple backups and check for recent backup based on just date and not time

--- mysql_backup.py
+++ mysql_backup.py
@@ -3,10 +3,7 @@
 
 ###################### Inputs ############################
 FREQUENCY_DAYS = 1
-# To avoid consdering archives that took long time to backup
-# but are recent as old files
-BUFFER_DAYS = 1/24 
-
+NUM_BACKUPS = 1
 COMPRESS = True
 BACKUP_PATH='/media/kunal/KBServerBkup/mysql'
 SYSTEM_DATABASES = ["performance_schema","information_schema","sys",
@@ -29,53 +26,48 @@ if not os.path.exists(BACKUP_PATH):
     raise ValueError(f"Backup path [{BACKUP_PATH}] doesn't exist... Exiting...")
 
 
-# Time to identify old backups
-recent_file_time = time.time() - FREQUENCY_DAYS * 24 * 60 * 60
-recent_file_time = time.strftime(time_format, time.localtime(recent_file_time))
+# Dates to identify old v/s new backups
+run_date = date.today().strftime("%Y%m%d")
 
-
-# Check if there is a backup more recent than above time
+# Check if there is a backup for above date
 current_files = os.listdir(BACKUP_PATH)
 
-new_files = [f for f in current_files
-             if re.search(".*_(\\d+)\\..*", f)
-                 and re.search(".*_(\\d+)\\..*", f).group(1) >= recent_file_time]
+new_file = [f for f in current_files
+            if re.search(".*_(\\d+)\\..*", f)
+                 and re.search(".*_(\\d+)\\..*", f).group(1)[:8] == run_date]
 
-if len(new_files) > 0:
-    print(f"mysql_backup.py: Recent backups found [{new_files}]\nExiting...")
-    sys.exit(0)
+if len(new_file) > 0:
+    print(f"mysql_backup.py: Recent backups found [{new_file}]")
+else:
+    # Databases to backup
+    databases = set(run_sql("SHOW DATABASES")["Database"]) - set(SYSTEM_DATABASES)
 
+    try:
+        for db in databases:
+            filename = f"{db}_{time.strftime(time_format, time.localtime(time.time()))}"
+            filename = os.path.join(BACKUP_PATH, filename)
 
-# Databases to backup
-databases = set(run_sql("SHOW DATABASES")["Database"]) - set(SYSTEM_DATABASES)
+            print(f"mysql_backup.py: Dumping database [{db}] to file [{filename}.*]")
 
-try:
-    for db in databases:
-        filename = f"{db}_{time.strftime(time_format, time.localtime(time.time()))}"
-        filename = os.path.join(BACKUP_PATH, filename)
+            if COMPRESS:
+                os.system(f"mysqldump --databases {db} | gzip -c > {filename}.gz")
+            else:
+                os.system(f"mysqldump --databases {db} > {filename}.sql")
+    except:
+        print("mysql_backup.py: Error backing up databases")
+        send_email(message = f"Failed to backup mysql databases at [{start_time}]",
+                  subject = f"kbserver: Failed to backup mysql [{start_time}]")
+        raise
 
-        print(f"mysql_backup.py: Dumping database [{db}] to file [{filename}.*]")
-
-        if COMPRESS:
-            os.system(f"mysqldump --databases {db} | gzip -c > {filename}.gz")
-        else:
-            os.system(f"mysqldump --databases {db} > {filename}.sql")
-except:
-    print("mysql_backup.py: Error backing up databases")
-    send_email(message = f"Failed to backup mysql databases at [{start_time}]",
-              subject = f"kbserver: Failed to backup mysql [{start_time}]")
-    raise
-
-print("mysql_backup.py: All databases successfully backed up")
+    print("mysql_backup.py: All databases successfully backed up")
 
 
 # Delete old backups
-old_file_time = time.time() - FREQUENCY_DAYS * 24 * 60 * 60 - BUFFER_DAYS * 24 * 60 * 60
-old_file_time = time.strftime(time_format, time.localtime(old_file_time))
+keep_file_min_date = (date.today() + timedelta(days = NUM_BACKUPS - 1)).strftime("%Y%m%d")
 
 old_files = [f for f in current_files
              if re.search(".*_(\\d+)\\..*", f)
-                 and re.search(".*_(\\d+)\\..*", f).group(1) < old_file_time]
+                 and re.search(".*_(\\d+)\\..*", f).group(1)[:8] < keep_file_min_date]
 for f in old_files:
     print(f"mysql_backup.py: Deleting old file [{f}]")
     try:
